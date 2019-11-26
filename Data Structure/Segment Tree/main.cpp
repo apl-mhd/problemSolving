824f8ec2cdb73ff4167869a1ac2dee772002e8ba
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2019-11-27 01:35:48 +0600

    segment tried again

--- Data Structure/Segment Tree/main.cpp
+++ Data Structure/Segment Tree/main.cpp
@@ -2,70 +2,82 @@
 #include <cstdio>
 #define  mx 100001
 
+using  namespace std;
 
-int arr[mx];
+int arr[mx]={0};
 int tree[mx*3] = {0};
 
-using namespace std;
 
 void  init(int node, int b, int e){
 
 
-    if(b==e){
+    if( b == e ){
+
         tree[node] = arr[b];
         return;
     }
 
 
-    int left = node*2;
-    int right = node*2+1;
-    int mid  = (b+e) / 2;
+    int left = node * 2;
+    int right = node * 2 + 1;
+    int  mid =  (b + e) / 2;
 
     init(left, b, mid);
     init(right, mid+1, e);
-
-    tree[node] = tree[left] +tree[right];
-
+    tree[node] = tree[left] + tree[right];
 
 }
 
+
+
 int query(int node, int b, int e, int i, int j){
 
-    if (i > e || j < b)
 
-        return 0;
 
-    if(i<=b && e<=j)
+    if(j< b || i > e)
+        return  0;
+
+    if(b >= i && j >= e){
+
         return tree[node];
 
-    int left = node *2;
-    int right = node * 2 +1;
-    int mid = (b + e) /2;
+    }
 
+
+    int left = node * 2;
+    int right = node * 2 + 1;
+    int mid = ( b + e )  / 2;
     return query(left, b, mid, i, j) + query(right, mid+1, e, i, j);
 
 }
 
 
-void  update(int node, int b, int e, int i, int nValue){
+void  update(int node, int b, int e, int i, int v){
 
-    if(i<b || i>e)
-        return;
 
-    if(b==e && b==i){
 
-        tree[node] = nValue;
-        return;
-    }
+        if( i < b ||  i  > e)
+            return;
 
-    int left = node * 2;
-    int right = node * 2 + 1;
-    int mid = (b+e)/2;
 
-    update(left, b, mid, i, nValue);
-    update(right, mid+1, e, i, nValue);
+        if(b==e & b==i){
 
-    tree[node] = tree[left] + tree[right];
+            tree[node] = v;
+            return;
+        }
+
+
+
+        int left = node * 2;
+        int right = node * 2 + 1;
+        int mid = (b + e) /2 ;
+
+
+        update(left, b, mid, i, v);
+        update(right, mid+1, e, i, v);
+
+        tree[node] =  tree[left] + tree[right];
+
 
 
 }
@@ -74,10 +86,14 @@ void  update(int node, int b, int e, int i, int nValue){
 int main() {
 
 
-    arr[1] = 0;
-    arr[2] = 0;
-    arr[3] = 0;
-    arr[4] = 0;
+
+
+    arr[1] = 1;
+    arr[2] = 2;
+    arr[3] = 3;
+    arr[4] = 4;
+
+
 
 
 
@@ -87,12 +103,12 @@ int main() {
 
         //cout<<i<<" :"<<tree[i]<<" "<<endl;
     }
-    cout<<endl;
 
-    update(1, 1, 4, 1, 1);
-    update(1, 1, 4, 2, 2);
-    update(1, 1, 4, 3, 3);
-    update(1, 1, 4, 4, 4);
+
+    update(1, 1, 4, 1, 10);
+    update(1, 1, 4, 2, 20);
+  //  update(1, 1, 4, 3, 3);
+//    update(1, 1, 4, 4, 4);
 
 
 
@@ -101,15 +117,19 @@ int main() {
     cout<<query(1, 1, 4, 1, 3)<<endl;
     cout<<query(1, 1, 4, 1, 4)<<endl;
 
+
+
+
     cout<<query(1, 1, 4, 2, 2)<<endl;
     cout<<query(1, 1, 4, 2, 3)<<endl;
     cout<<query(1, 1, 4, 2, 4)<<endl;
 
-
+/*
     cout<<query(1, 1, 4, 3, 3)<<endl;
     cout<<query(1, 1, 4, 3, 4)<<endl;
 
     cout<<query(1, 1, 4, 4, 4)<<endl;
+     */
 
 
 
