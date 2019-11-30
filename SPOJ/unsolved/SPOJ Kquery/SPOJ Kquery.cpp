e5d1ccad73671808f7172321837e1b94ea762a61
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2019-11-30 11:10:15 +0600

    kquery

---
+++ SPOJ/unsolved/SPOJ Kquery/SPOJ Kquery.cpp
@@ -0,0 +1,52 @@
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+
+
+using namespace std;
+#define pb push_back
+
+typedef vector<int> vi;
+
+
+
+
+int main() {
+
+    vi v = { 10, 9, 8, 7, 6, 5, 4, 3, 2};
+
+    vi::iterator it;
+
+
+
+    sort(v.begin(), v.begin()+3);
+
+    for (it = v.begin(); it !=v.end(); it++) {
+
+        cout<<*it<<" ";
+    }
+
+
+
+
+
+
+    return 0;
+}
