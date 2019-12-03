425145fba7b38e7b7cdf9532adeb6421699f83b6
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2019-12-03 21:39:22 +0600

    ad hoc please do not look at these problem I want back CP again :)

---
+++ UVA/Relational Operator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+
+
+using  namespace std;
+
+int main() {
+
+
+    int t;
+
+    cin>>t;
+
+
+    for (int i = 0; i <t; ++i) {
+
+        int a,b;
+        cin>>a>>b;
+
+        if(a>b)
+            cout<<">"<<endl;
+        else if(a<b)
+            cout<<"<"<<endl;
+        else
+            cout<<"="<<endl;
+    }
+
+
+
+
+    return 0;
+}
