320fe66c77af919036cd4f601901093b77a49108
Apel Mahmud <apl.mhd@gmail.com>
2024-12-16 14:40:25 +0600

    leetcode array problem

---
+++ LeetCode/442_find_all_duplicates_in_an_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicates(self, nums: list[int]) -> list[int]:
+
+        result = []
+        arr_size = len(nums) + 1
+        seen = [0] * (arr_size)
+
+        for i in nums:
+            seen[i] += 1
+
+        for i in range(1, arr_size):
+            if seen[i] > 1:
+                result.append(i)
+
+        return result
